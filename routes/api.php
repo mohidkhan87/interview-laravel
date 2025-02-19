a48af6ead8652264eb7ad150cce3759a3bcd41b9
mohidkhan87 <mohidkhan87@gmail.com>
2025-02-19 16:05:52 +0500

    search screens

--- routes/api.php
+++ routes/api.php
@@ -3,4 +3,4 @@ use App\Http\Controllers\ScreenController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/screens', [ScreenController::class, 'index']);
-Route::get('/search_screens', [ScreenController::class, 'search']);
+Route::post('/search_screens', [ScreenController::class, 'search']);
